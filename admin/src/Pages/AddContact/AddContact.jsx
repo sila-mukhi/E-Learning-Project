f00c30bc45becc232b0daaca39bd6077eba15656
sila-mukhi <silamukhi165@gmail.com>
2024-11-05 16:12:48 +0530

    Update AddContact.jsx

--- admin/src/Pages/AddContact/AddContact.jsx
+++ admin/src/Pages/AddContact/AddContact.jsx
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 
 
 const AddContact = () => {
-    const url = "http://localhost:4300";
+    const url = "https://e-learning-project-backend.onrender.com";
     
     const [data, setData] = useState({
         name: "",
