b9abc90cce45bd9d66168c5352bfd3be800c166e
sila-mukhi <silamukhi165@gmail.com>
2024-11-06 11:16:14 +0530

    Update Contact.jsx

--- elearning/src/Pages/Contact/Contact.jsx
+++ elearning/src/Pages/Contact/Contact.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ContactUs from '../../Components/ContactUs/ContactUs'
-
+import { assets } from '../../assets/assets';
 const Contact = () => {
   return (
     // <!-- Header Start -->
     <>
-    <div className="container-fluid bg-primary py-5 mb-5 page-header">
+    <div className="container-fluid bg-primary py-5 mb-5 page-header" style={{  background: linear-gradient(rgba(24, 29, 56, .7), rgba(24, 29, 56, .7)),`url(${assets.carousel_1})`}}>
         <div className="container py-5">
             <div className="row justify-content-center">
                 <div className="col-lg-10 text-center">
